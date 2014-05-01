58e5fe5c96db7c2f44776bf2bbef95da3a228299
Hank Beaver <dev+hank@blinqmedia.com>
2014-04-30 22:47:42 -0400

    Initial commit

---
+++ MySQLReplicationStatus.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+
+import re
+import commands
+
+SLAVE_IO = "cat /tmp/slave.status | grep Slave_IO_Running | cut -d ':' -f 2"
+SLAVE_SQL = "cat /tmp/slave.status | grep Slave_SQL_Running | cut -d ':' -f 2"
+SECS_BEHIND = "cat /tmp/slave.status | grep Seconds_Behind_Master | cut -d ':' -f 2"
+OK = 0
+ERR = 1
+SECS_BEHIND_ERR = -1
+
+class MysqlSlaveMonitor:
+    def __init__(self, agent_config, checks_logger, raw_config):
+        self.agent_config = agent_config
+        self.checks_logger = checks_logger
+        self.raw_config = raw_config
+
+        if self.agent_config is None:
+          self.set_default_config()
+
+        if ('mysql_status' not in self.agent_config):
+          self.set_default_config()
+
+    def set_default_config(self):
+        self.agent_config = {}
+        self.agent_config['mysql_status'] = {'user': 'root', 'password': ''}
+
+
+    def run(self):
+        stats = {}
+        stats['status'] = OK
+        # generate the status
+        user = self.agent_config['mysql']['user']
+        password = self.agent_config['mysql']['password']
+        command = "mysql -u%(user) -p%(password) -e 'SHOW SLAVE STATUS\G' > /tmp/slave.status" %  locals()
+        commands.getoutput(MYSQL_SLAVE_STATUS)
+        slave_io_status = commands.getoutput(SLAVE_IO).strip()
+        slave_sql_status = commands.getoutput(SLAVE_SQL).strip()
+        slave_secs_behind = commands.getoutput(SECS_BEHIND).strip()
+
+        if slave_io_status != 'Yes':
+            stats['status'] = ERR
+        elif slave_sql_status != 'Yes':
+            stats['status'] = ERR
+
+        if slave_secs_behind == 'NULL' :
+            stats['seconds_behind_master'] = SECS_BEHIND_ERR
+            stats['status'] = ERR
+        else:
+            stats['seconds_behind_master'] = int(slave_secs_behind)
+
+        return stats
+
+if __name__ == '__main__':
+    mysql_slave = MysqlSlaveMonitor(None, None, None)
+    print mysql_slave.run()
--- README.md
+++ README.md
@@ -1,4 +1,36 @@
-server_density_mysql_replication_status
-=======================================
+#MySqlReplicationStatus Server Density Plugin
 
-Tested and simple MySQL replication status Server Density plugin
+Tested and simple MySQL replication status Server Density plugin that uses `mysql` client and the shell.
+
+#Metrics
+
+The plugin will report two metrics, `seconds_behind_master` and `status`. 
+
+## 'seconds_behind_master' 
+
+This metric is a good indication of how healthy your replication is AND if it has failed. If the replication has failed, usually the `Seconds_Behind_Master` will be `NULL`. This metric reports that as `-1`.
+
+## 'status'
+
+The second metric is informative based on whether `Slave_IO_Running` or `Slave_SQL_Running` report 'Yes'.
+
+
+Both of these metrics allows you to easily set thresholds in Server Density to alert if the slave is behind OR if it stops. 
+
+##Assumptions
+
+* Requires `mysql` in `$PATH`
+* You must set the user and password in your server density config file. This user must have SUPER or REPLICATION CLIENT privilege (so yes this script presumes a high level of access, but it runs on the DB server directly anyway).
+* It can write a temp file to `/tmp/slave.status`
+
+##Installation
+
+# Copy `MySQLReplicationStatus.py` plugin to your `sd-agent` plugins folder (`/usr/bin/sd-agent/plugins`). Create the plugins folder if it doesn't exist.
+
+# Add `mysql_status` section to your Server Density config.
+
+    [mysql_status]
+    user: 'replication'
+    password: 'password1'
+
+# Restart the agent.
